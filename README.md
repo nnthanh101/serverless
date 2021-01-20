7fca61a60cfdf0ef1bac7a5800fd169da82f9088
Thanh Nguyen <nnthanh101@gmail.com>
2021-01-20 20:01:31 +0700

    🎯 https://devsecops.job4u.io/en/serverless/sam-local

---
+++ README.md
@@ -0,0 +1,11 @@
+# Serverless
+
+To build Web-apps & Mobile-apps that scale automatically on low-cost, fully-managed Serverless architecture.
+
+## 1. SAM-REST
+
+Provide a guidance & inspiration on how to use AWS SAM to develop a Serverless RESTful API.
+
+* [x] [AWS Local Development](https://devsecops.job4u.io/en/serverless/sam/init/)
+    
+* [x] `./deploy.sh` 
---
+++ sam-rest/deploy.sh
@@ -0,0 +1,28 @@
+export PROJECT_ID=sam-rest
+export AWS_ACCOUNT=$(aws sts get-caller-identity | jq -r '.Account' | tr -d '\n')
+export AWS_REGION=${AWS_REGION:-"ap-southeast-1"}
+
+export S3_BUCKET=${PROJECT_ID}-${AWS_ACCOUNT}-${AWS_REGION}
+## aws s3api create-bucket --bucket ${S3_BUCKET} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION} || true
+aws s3api create-bucket --bucket ${S3_BUCKET} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION}
+aws s3api put-bucket-versioning --bucket ${S3_BUCKET} --versioning-configuration Status=Enabled
+
+sam deploy --stack-name ${PROJECT_ID}                   \
+           --region ${AWS_REGION} --confirm-changeset --no-fail-on-empty-changeset \
+           --capabilities CAPABILITY_NAMED_IAM          \
+           --s3-bucket ${S3_BUCKET} --s3-prefix backend \
+           --config-file samconfig.toml                 \
+           --no-confirm-changeset                       \
+           --tags \
+              Project=${PROJECT_ID}
+
+echo "Testing AWS cloud resources from local development environments ..."
+./get-vars.py sam-rest > local-env.json
+          
+sam local invoke --env-vars local-env.json    \
+          getAllItemsFunction                 \
+          -e events/employee-service/event-get-all-items.json
+         #  getByIdFunction                     \
+         #  -e events/employee-service/event-get-by-id.json
+         #  putItemFunction                     \
+         #  -e events/employee-service/event-post-item.json         
