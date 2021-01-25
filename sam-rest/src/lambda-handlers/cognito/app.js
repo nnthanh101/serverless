1764d3094b6efc1a54fea0ceab95b11a3e955712
Thanh Nguyen <nnthanh101@gmail.com>
2021-01-25 14:41:36 +0700

    🎯 cognito - 1st-Init

---
+++ sam-rest/src/lambda-handlers/cognito/app.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+exports.lambdaHandler = async (event, context, callback) => {
+    let newScopes = event.request.groupConfiguration.groupsToOverride.map(item => `${item}-${event.callerContext.clientId}`)
+  
+    event.response = {
+      "claimsOverrideDetails": {
+        "claimsToAddOrOverride": {
+          "scope": newScopes.join(" "),
+        }
+      }
+    };
+  
+    callback(null, event)
+  }
