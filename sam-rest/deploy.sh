a02ef2c438cbac6061e910db5ae0b26a7d90ef38
NNThanh101 <nnthanh101@gmail.com>
2021-01-28 12:13:20 +0000

    cloud9.sh

--- sam-rest/deploy.sh
+++ sam-rest/deploy.sh
@@ -41,16 +41,16 @@ sam local invoke --env-vars local-env.json    \
 echo "Integration Test ..."
 AWS_SAM_STACK_NAME=${PROJECT_ID} npm run integ-test
 
-echo "Template to create a cognito backend to use with HTTP APIs JWT authorizer. Includes option to add Cognito User Groups as custom scopes for route access."
-sam deploy --stack-name ${PROJECT_ID}                   \
-           --template-file template.cognito.yml         \
-           --region ${AWS_REGION} --confirm-changeset --no-fail-on-empty-changeset \
-           --capabilities CAPABILITY_NAMED_IAM          \
-           --s3-bucket ${S3_BUCKET} --s3-prefix backend \
-           --config-file samconfig.toml                 \
-           --no-confirm-changeset                       \
-           --tags \
-              Project=${PROJECT_ID}      
+# echo "Template to create a cognito backend to use with HTTP APIs JWT authorizer. Includes option to add Cognito User Groups as custom scopes for route access."
+# sam deploy --stack-name ${PROJECT_ID}                   \
+#            --template-file template.cognito.yml         \
+#            --region ${AWS_REGION} --confirm-changeset --no-fail-on-empty-changeset \
+#            --capabilities CAPABILITY_NAMED_IAM          \
+#            --s3-bucket ${S3_BUCKET} --s3-prefix backend \
+#            --config-file samconfig.toml                 \
+#            --no-confirm-changeset                       \
+#            --tags \
+#               Project=${PROJECT_ID}      
 
 ## Danger!!! Cleanup
 # echo "Cleanup ..."
